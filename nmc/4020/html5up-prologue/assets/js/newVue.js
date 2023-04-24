6e14d7e14bdda4f36cc750b6f6fb3440f93f4e9d
sao91172 <75396894+sao91172@users.noreply.github.com>
2023-04-24 19:25:44 -0400

    yo

--- nmc/4020/html5up-prologue/assets/js/newVue.js
+++ nmc/4020/html5up-prologue/assets/js/newVue.js
@@ -16,7 +16,53 @@ var portfolio = new Vue ({
     el: '#portfolio',
     data: {
         portfolioTitle: "Say something about your portfolio",
-        image1:
-        imgText1: "ImageOne"
+        image1: "images/iteration1.jpg",
+        imgText1: "Uli Art",
+        image2: "images/comp2.jpg",
+        imgText2: "Grids Generate Form",
+        image3: "images/jammedprinter.jpg",
+        imgText3: "A \"Jammed\" Printer",
+        image4: "images/Letterform_Exercise_2_template2.jpg",
+        imgText4: "Letterform",
+        image5: "images/new_Letterform_Exercise_2_template.jpg",
+        imgText5: "Letterform v2",
+        image6: "images/shaperemix_4.jpg",
+        imgText6: "Shape Remix",
+       
+
+    }
+})
+var list = new Vue({
+    el: '#list',
+    data: {
+        link5: 'List',
+        header: "Favorite Pefumes" ,
+        favorites: ['Si Passione - Armani ', 'I want choo-jimmy choo','Donna born in Roma- Valentino','Mon Guerlain - Guerlain']
+    }
+})
+
+var conditional = new Vue ({
+    el: '#conditional',
+    data: {
+        teacherName: '',
+        birthDate: '',
+    }
+})
+
+var getColor = new Vue ({
+    el: '#function',
+    data: {
+        bgColor: '',
+        w: '',
+        colors: ['red', 'green', 'blue'],
+        widths: ['400px', '500px', '600px']
+    },
+    methods: {
+        changeColor(color) {
+            this.bgColor = color;
+        },
+        changeWidth(width) {
+            this.w = width;
+        }
     }
 })
