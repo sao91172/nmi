4dd37deb5f7b88a353bcffb7fbf495773c9603ea
sao91172 <75396894+sao91172@users.noreply.github.com>
2023-05-10 02:32:44 -0400

    yo

--- nmc/4020/colorScheme/palette.js
+++ nmc/4020/colorScheme/palette.js
@@ -1,37 +1,38 @@
 const { createApp } = Vue;
-
+let button = document.querySelector('#generate');
 createApp({
   data() {
     return {
-      colorData: [],
+      colors: [],
       message:'',
       showSuccessAlert: false,
     };
   },
   methods: {
     generateHex() {
-      this.colorData = []
-    //   this.showSuccessAlert=false
+      this.colors = []
+      this.showSuccessAlert=false
+    let hexColor = '';
       const usedColors = new Set() // Keep track of used colors
-      while (this.colors.length < 6) {
+      while (this.colors.length < 5) {
         let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        let hexColor = '#' + randomColor
+        hexColor = '#' + randomColor
         if (!usedColors.has(hexColor)) { // Check if color has already been added
           this.colors.push(hexColor)
           usedColors.add(hexColor)
         }
       }
-    },    
-    handleSpacebar(e) {
+    },
+    handleSpaceAndEnter(e) {
       if (e.code === 'Space' || e.code === 'Enter') {
         // Your code to execute when the spacebar is pressed
-        this.fetchData()
+        this.generateHex()
       }
     },
     handleCopy(color){
       if (navigator.clipboard) {
         navigator.clipboard.writeText(color);
-        this.message='copied'+color
+        this.message='Color '+ color + ' copied to clipboard!'
         this.showSuccessAlert=true
       } else {
         var textarea = document.createElement('textarea');
@@ -43,7 +44,7 @@ createApp({
         textarea.select();
         try {
           document.execCommand('copy');
-          this.message='copied'+color
+          this.message='Color '+ color + ' copied to clipboard!'
           this.showSuccessAlert=true
         } catch (err) {
           console.error('Could not copy text: ', err);
@@ -52,60 +53,37 @@ createApp({
       }
       
     },
-    handleClick(event) {
+    handleClick(e) {
       // Check if the clicked element is a link
-      if (event.target.tagName !== 'A' && !event.target.closest('.colored')) {
+      if (e.target.tagName !== 'A' && !e.target.closest('.colored')) {
         // Your code to execute when a non-link element is clicked goes here
        this.showSuccessAlert=false
       }
     }
-  },
-  mounted() {
+      },
+       
+  mount() {
     document.addEventListener('click', this.handleClick);
-    this.calculate();
-    document.addEventListener('keydown', this.handleSpacebar);
+    this.generateHex();
+    document.addEventListener("keypress", this.handleSpaceAndEnter);
   },
   beforeUnmount() {
     document.removeEventListener('click', this.handleClick);
-    document.removeEventListener('keydown', this.handleSpacebar);
+    document.removeEventListener("keypress", this.handleSpaceAndEnter);
   },
 
-}).mount("#app");
-
-
-// const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-// const colorData = ref([])
-
-// const generateHex = () =>{
-//  let hexColor = ''
-//  for (let i = 0; i < 6; i++){  
-//    const num = Math.floor(Math.random() * digits.length)
-//    hexColor += digits[num]
-//  }
-//  return hexColor
-//  }
-
-// onMounted( () =>{
-//  window.addEventListener("keypress", e => {
-//        if(e.code === 'Space' || e.code === 'Enter'){
-//      fetchData()
-   
-//    }
+  //  fetchData () {
+  //   const hexColor= generateHex()
+  //   try{
+  //     const res = await fetch(`https://www.thecolorapi.com/scheme?hex=${hexColor}&mode=analogic`)
+  //     const jsonResponse = await res.json()
+  //     colorData.value = jsonResponse.colors
+  //     console.log(colorData.value)
+  //   }catch(error){
+  //     console.log(error)
+  //   }
+  //}
   
-//    });
+  
 
-// })
-
-// const fetchData = async () =>{
-//  const hexColor= generateHex()
-//  try{
-//    const res = await fetch(`https://www.thecolorapi.com/scheme?hex=${hexColor}&mode=analogic`)
-//    const jsonResponse = await res.json()
-//    colorData.value = jsonResponse.colors
-//    console.log(colorData.value)
-//  }catch(error){
-//    console.log(error)
-//  }
-// }
-
-// fetchData()
+}).mount("#app");
